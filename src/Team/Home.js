d125f5215c23fb050f0aa4e1eb31455df1a03b97
ppodds <oscar20020629@gmail.com>
2024-03-20 17:50:33 +0800

    feat: short cut for jump to page's start/end

--- src/Team/Home.js
+++ src/Team/Home.js
@@ -1,80 +1,100 @@
-import { NativeBaseProvider, Box, HStack, Image, IconButton, Tooltip} from "native-base";
-import '..//App.css';
-import bg from './bg.png';
+import {
+  NativeBaseProvider,
+  Box,
+  HStack,
+  Image,
+  IconButton,
+  Tooltip,
+} from "native-base";
+import "..//App.css";
+import bg from "./bg.png";
 import { BsInfo } from "react-icons/bs";
 import { RiCustomerService2Fill } from "react-icons/ri";
 import React, { useState, useEffect } from "react";
-import MemberCard from "./memberCard"
-import { Link} from "react-router-dom";
+import MemberCard from "./memberCard";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const [show, setShow] = useState(false);
-  function handleKeyDown(event) {
-    if(event.key === 'ArrowDown' || event.key === 'ArrowRight'){
-        window.location.href = '/uiux-profile/#/chen';
-    }
-  }
-  useEffect(() => {
-  
-    window.addEventListener('keydown', handleKeyDown);
-  
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []); // Empty dependency array means this effect runs once on mount and clean up on unmount
-    
-    return (
-        <NativeBaseProvider>
-          <Box w={'full'} h={'full'} >
-          {/* 'Background */}
-            <Image w={'100%'} height={'100%'} source={bg} alt="bg" position={'fixed'} zIndex={-1}></Image>
-                    
-              <MemberCard show={show} />
+  return (
+    <NativeBaseProvider>
+      <Box w={"full"} h={"full"}>
+        {/* 'Background */}
+        <Image
+          w={"100%"}
+          height={"100%"}
+          source={bg}
+          alt="bg"
+          position={"fixed"}
+          zIndex={-1}
+        ></Image>
 
-            {/* Footer */}
-            <Box className='footer' height={'20px'} paddingX={2} position={'fixed'} bottom={6}>
-              <HStack>
-                <Tooltip label="使用教學" bg="gray.600:alpha.30" color="gray" placement="top">
-                  <IconButton borderRadius='10rem' icon={<BsInfo size={'27px'} />}
+        <MemberCard show={show} />
+
+        {/* Footer */}
+        <Box
+          className="footer"
+          height={"20px"}
+          paddingX={2}
+          position={"fixed"}
+          bottom={6}
+        >
+          <HStack>
+            <Tooltip
+              label="使用教學"
+              bg="gray.600:alpha.30"
+              color="gray"
+              placement="top"
+            >
+              <IconButton
+                borderRadius="10rem"
+                icon={<BsInfo size={"27px"} />}
+                _icon={{
+                  color: "#8E9D7D",
+                  size: "md",
+                }}
+                _hover={{
+                  bg: "cyan.600:alpha.30",
+                }}
+                _pressed={{
+                  bg: "cyan.600:alpha.20",
+                }}
+                onPress={() => {
+                  setShow(!show);
+                }}
+              />
+            </Tooltip>
+            <Tooltip
+              label="聯絡我們"
+              bg="gray.600:alpha.30"
+              color="gray"
+              placement="top"
+            >
+              <Link to="/contact">
+                <IconButton
+                  borderRadius="10rem"
+                  icon={<RiCustomerService2Fill size={"27px"} />}
                   _icon={{
                     color: "#8E9D7D",
-                    size: "md"
-                  }} 
-                  _hover={{
-                    bg: "cyan.600:alpha.30", 
-                  }} 
-                  _pressed={{
-                    bg: "cyan.600:alpha.20",
-                  }} 
-                  onPress={() => {
-                    setShow(!show);
+                    size: "md",
+                    borderColor: "#8E9D7D",
+                    borderWidth: "3px",
+                    borderStyle: "solid",
                   }}
-                  />
-                </Tooltip>
-                <Tooltip label="聯絡我們" bg="gray.600:alpha.30" color="gray" placement="top">
-                  <Link to="/contact">
-                  <IconButton borderRadius='10rem' icon={<RiCustomerService2Fill size={'27px'} />}
-                  _icon={{
-                    color: "#8E9D7D",
-                    size: "md", 
-                    borderColor: '#8E9D7D',
-                    borderWidth: '3px', 
-                    borderStyle: 'solid'
-                  }} 
                   _hover={{
-                    bg: "cyan.600:alpha.30", 
-                  }} 
+                    bg: "cyan.600:alpha.30",
+                  }}
                   _pressed={{
                     bg: "cyan.600:alpha.20",
-                  }} 
-                  />
-                  </Link>
-                </Tooltip>
-              </HStack>
-            </Box>
-          </Box>
-        </NativeBaseProvider>
-    );
-}
+                  }}
+                />
+              </Link>
+            </Tooltip>
+          </HStack>
+        </Box>
+      </Box>
+    </NativeBaseProvider>
+  );
+};
 
 export default Home;
