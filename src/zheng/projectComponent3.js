d0a9704cb993daf2b2874f35e3e53c727bbd3fb0
1989ONCE <susanloveselena@gmail.com>
2024-03-26 23:09:07 +0800

    fix: zhengProject component position adjustment

--- src/zheng/projectComponent3.js
+++ src/zheng/projectComponent3.js
@@ -9,9 +9,9 @@ import { Link } from "react-router-dom";
 function ProjectComponent3() {
 
     return (
-        <Center w={'80%'} h={'493px'} alignItems={'flex-start'}>
+        <Center style={{transform: [{translateY: '-60px'}]}} w={'80%'} h={'334.9px'} alignItems={'flex-start'}>
             <Box flexDirection={'row'} w={'full'} h={'full'} space={30} flexWrap={'wrap'} >
-                    <VStack space={5} w={{sm: 'full', lg: '50%'}} h={'full'} alignContent={'center'} alignItems={'center'} justifyContent={'center'} justifyItems={'center'}> 
+                    <VStack space={5} w={{sm: 'full', lg: '50%'}} h={'493px'} alignContent={'center'} alignItems={'center'} justifyContent={'center'} justifyItems={'center'}> 
                         <Box w={'full'} h={'45%'} alignSelf={'flex-start'} alignItems={'center'}>
                             <Image w={'full'} h={'90%'} resizeMode="contain" source={p1} alt="project1" />
                             <Link target="_blank" to='https://youtu.be/nsszk8THTUI?si=LTdimSV1zRcb_cMp' style={{textDecorationColor: '#8E9D7D'}}><Text color={'#8E9D7D'} fontFamily={"cwTeXKai"} fontWeight={800} fontSize={'1.3rem'}>我聽見另一種聲音</Text></Link>
@@ -21,7 +21,7 @@ function ProjectComponent3() {
                             <Link target="_blank" to='https://youtu.be/OzKp9HN0nDE' style={{textDecorationColor: '#8E9D7D'}}><Text color={'#8E9D7D'} fontFamily={"cwTeXKai"} fontWeight={800} fontSize={'1.3rem'}>捐助年菜 創造善的循環</Text></Link>
                         </Box>
                     </VStack>
-                    <VStack w={{sm: 'full', lg: '50%'}} h={{sm: 'full', lg: '95%'}}> 
+                    <VStack w={{sm: 'full', lg: '50%'}} h={{sm: '300px', lg: '454px'}}> 
                         <Box w={'full'} h={'full'} alignSelf={'flex-start'} alignItems={'center'}>
                             <Image w={'full'} h={'full'} resizeMode="contain" source={p3} alt="project3" />
                             <Link target="_blank" to='https://drive.google.com/file/d/1NH_2G3cW22wRzHIpVygo_0eEgeNvXKxT/view?usp=drive_link' style={{textDecorationColor: '#8E9D7D'}}><Text color={'#8E9D7D'} fontFamily={"cwTeXKai"} fontWeight={800} fontSize={'1.3rem'}>公益傳播大使成果冊</Text></Link>
